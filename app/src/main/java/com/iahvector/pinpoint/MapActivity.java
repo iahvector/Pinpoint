b8313a6485b497d279e2440eee8d67f3e5d5f40a
iahvector <iah.vector@gmail.com>
2016-05-25 23:55:38 +0200

    Add my location button

--- app/src/main/java/com/iahvector/pinpoint/MapActivity.java
+++ app/src/main/java/com/iahvector/pinpoint/MapActivity.java
@@ -9,11 +9,13 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -79,6 +81,14 @@ public class MapActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        FloatingActionButton myLocation = (FloatingActionButton) findViewById(R.id.myLocation_fab);
+        myLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestLocationAction(ANIMATE_TO_MY_LOCATION_REQUEST_CODE, true);
+            }
+        });
     }
 
     @Override
